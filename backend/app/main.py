0aab214a729f4c19a9be7ce04b031913c3945a1f
Mansi <mjoshi112003@gmail.com>
2025-07-29 19:40:02 +0530

    Cleanup pycache and update .gitignore to ignore .pyc files

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI
 from app.database import engine, Base
 from app.models import user            # ensure models are imported
 from app.routers import auth           # your auth endpoints
-
+from app.routers import classroom   
 Base.metadata.create_all(bind=engine)  # creates tables defined in models
 
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+app.include_router(classroom.router)  
