c12c80a35e37b8d544248d5fe003b4743b852fab
apekshagangurde <apekshagangurde08@gmail.com>
2025-09-26 18:18:50 +0530

    swagger login auth configure

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -2,9 +2,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
 from app.database import get_db
-from app.schemas.user import UserCreate, UserLogin
+from app.schemas.user import UserCreate
 from app.crud.user import get_user_by_username, create_user, verify_password ,get_user_by_email
-from app.auth.auth_handler import create_access_token
 from app.crud.jwt import create_access_token
 
 from fastapi.security import OAuth2PasswordRequestForm
@@ -22,9 +21,9 @@ def register(user: UserCreate, session: Session = Depends(get_db)):
     return create_user(session, user)
 
 @router.post("/login")
-def login(user: UserLogin, session: Session = Depends(get_db)):
-    db_user = get_user_by_username(session, user.username)
-    if not db_user or not verify_password(user.password, db_user.hashed_password):
+def login(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(get_db)):
+    db_user = get_user_by_username(session, form_data.username)
+    if not db_user or not verify_password(form_data.password, db_user.hashed_password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
     token = create_access_token({"sub": db_user.username})
     return {"access_token": token, "token_type": "bearer"}
