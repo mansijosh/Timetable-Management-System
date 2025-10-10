fcf9adc2d39ab6d13145519331daf8290b95215d
apeksha gangurde <100061307+apekshagangurde@users.noreply.github.com>
2025-10-10 19:08:06 +0530

    format all files (#31)

--- frontend/src/routes/faculty/+page.svelte
+++ frontend/src/routes/faculty/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-    let message = "Hello Faculty";
-  </script>
-  
-  <div class="flex items-center justify-center min-h-screen bg-gray-100">
-    <h1 class="text-3xl font-bold">{message}</h1>
-  </div>
-  
+	let message = 'Hello Faculty';
+</script>
+
+<div class="flex min-h-screen items-center justify-center bg-gray-100">
+	<h1 class="text-3xl font-bold">{message}</h1>
+</div>
