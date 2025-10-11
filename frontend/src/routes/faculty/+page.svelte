ce69872a6a6a4c0688633a9f556187f2cc58e867
apekshagangurde <apekshagangurde08@gmail.com>
2025-10-11 12:58:18 +0530

    icons added for faculty and departemnt

--- frontend/src/routes/faculty/+page.svelte
+++ frontend/src/routes/faculty/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { Users } from 'lucide-svelte';
 	import type { PageData } from './$types';
 
 	interface Props {
@@ -11,9 +12,11 @@
 <div class="min-h-screen bg-gray-100 p-8">
 	<div class="mx-auto max-w-7xl">
 		<div class="mb-6 rounded-lg bg-white p-6 shadow-md">
-			<h1 class="mb-2 text-3xl font-bold" style="font-family: 'Poppins', sans-serif;">
-				Faculties
-			</h1>
+			
+			<h1 class="mb-2 text-3xl font-bold flex items-center gap-2" >
+	<Users  class="w-8 h-8 p-1" />
+	Faculties
+</h1>
 			<p class="text-gray-600">Manage all faculties and their details</p>
 		</div>
 
