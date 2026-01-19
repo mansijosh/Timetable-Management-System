3726e178fbfc7785dbf2df9899f2eb2a742c2f52
CodeSrushti-3105 <srushtiahire3105@gmail.com>
2026-01-19 20:30:29 +0530

    returned role meatadata

--- backend/app/routers/role.py
+++ backend/app/routers/role.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
 from app.database import get_db
 from app.schemas.role import RoleCreate, RoleRead
-from app.schemas.utils import Message
+from app.schemas.utils import Message, DeleteResponse
 from app.crud.role import create_role, get_roles, get_role_by_id, delete_role
 from app.crud.deps import get_current_user
 
@@ -23,9 +23,15 @@ def read_role(role_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Role not found")
     return role
 
-@router.delete("/{role_id}", response_model=Message)
+@router.delete("/{role_id}", response_model=DeleteResponse)
 def remove_role(role_id: int, db: Session = Depends(get_db)):
-    role = delete_role(db, role_id)
+    role = get_role_by_id(db, role_id)
     if not role:
         raise HTTPException(status_code=404, detail="Role not found")
-    return Message(message="Role deleted successfully")
+    deleted_data = {
+                     "id" : role.id,
+                     "role_name": role.role_name
+                   }
+    delete_role(db, role_id)
+    return DeleteResponse(message="Role deleted successfully",
+                          data = deleted_data)
