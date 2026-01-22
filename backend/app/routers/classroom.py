065f2e8942d7f8f76adb122ed9c9a8c89d72585b
Srushti Ahire <158837536+CodeSrushti-3105@users.noreply.github.com>
2026-01-22 17:04:08 +0530

    Include deleted classroom metadata in Classroom DELETE API response (#93)
    * returned classroom metadata
    
    * made variable consistent
    
    * removed variable shadowing
    
    * changes in variable name
    
    * Implemented DeleteClassroomResponse schema and updated delete_classroom endpoint to return structured response with classroom metadata.
    
    ---------
    
    Co-authored-by: apekshagangurde <apekshagangurde08@gmail.com>

--- backend/app/routers/classroom.py
+++ backend/app/routers/classroom.py
@@ -3,8 +3,8 @@ from sqlmodel import Session, select
 from app.database import get_db
 from app.models.classroom import Classroom
 from app.models.department import Department
-from app.schemas.classroom import ClassroomCreate, ClassroomRead, ClassroomUpdate
-from app.schemas.utils import Message
+from app.schemas.classroom import ClassroomCreate, ClassroomRead, ClassroomUpdate,DeleteClassroomResponse
+from app.schemas.utils import DeleteResponse
 from app.crud.deps import get_current_user
 
 router = APIRouter()
@@ -54,12 +54,23 @@ def update_classroom(classroom_id: int, classroom: ClassroomUpdate, session: Ses
     session.commit()
     session.refresh(db_classroom)
     return db_classroom
-
-@router.delete("/{classroom_id}", response_model=Message)
-def delete_classroom(classroom_id: int, session: Session = Depends(get_db), current_user = Depends(get_current_user)):
+@router.delete("/{classroom_id}", response_model=DeleteClassroomResponse)
+def delete_classroom(
+    classroom_id: int,
+    session: Session = Depends(get_db),
+    current_user=Depends(get_current_user)
+):
     db_classroom = session.get(Classroom, classroom_id)
     if not db_classroom:
         raise HTTPException(status_code=404, detail="Classroom not found")
+
+    
+    classroom_public = ClassroomRead.model_validate(db_classroom)
+
     session.delete(db_classroom)
     session.commit()
-    return Message(message="Classroom deleted successfully")
+
+    return DeleteClassroomResponse(
+        message="Classroom deleted successfully",
+        data=classroom_public
+    )
